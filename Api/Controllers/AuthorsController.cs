4f3ba05a69461fd29ea3aff75838a0a3fff922c2
ailci <ali.ilci@gmx.de>
2025-02-18 15:38:13 +0100

    AuthorId

--- Api/Controllers/AuthorsController.cs
+++ Api/Controllers/AuthorsController.cs
@@ -21,10 +21,18 @@ public class AuthorsController : ControllerBase
 
     #region GET
 
+    [HttpGet(Name = "GetAuthors")]
     public async Task<IActionResult> Get()
     {
         var authorDtos = await _serviceManager.AuthorService.GetAuthorsAsync();
         return Ok(authorDtos);
+    }
+
+    [HttpGet("{id:guid}", Name = "GetAuthor")]  // => localhost:1234/api/authors/{id}
+    public async Task<IActionResult> GetAuthor(Guid id)
+    {
+        var authorDto = await _serviceManager.AuthorService.GetAuthorAsync(id);
+        return Ok(authorDto);
     }
 
     #endregion
