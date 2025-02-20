efc520adf1a87cd408865e3e5274acfd3bd0fce0
ailci <ali.ilci@gmx.de>
2025-02-20 14:42:21 +0100

    overview

---
+++ Client.UI/Pages/Author/Overview.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Client.UI.Pages.Author
+{
+    public class OverviewModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
